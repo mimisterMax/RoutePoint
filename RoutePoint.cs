cb4eef4f2cf559782e24ae65fc24c1d4353ab3ac
doter <152766404+mimisterMax@users.noreply.github.com>
2024-04-21 10:31:24 +0900

    Add files via upload

---
+++ RoutePoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoutePoint : MonoBehaviour
+{
+    public RoutePoint nextPoint;
+    public GameObject model;
+    public bool isActiveForPlayer;
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+    }
+}
